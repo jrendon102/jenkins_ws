5aa05cac67b0a9befe6372862ae6041c5f2302eb
Julian A Rendon <julianrendon514@gmail.com>
2025-06-12 14:28:29 -0400

    Add cpp src code which takes in an argument and greets people.

---
+++ src/greet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc > 1)
+    {
+        std::cout << "Hello, " << argv[1] << "!\n";
+    }
+    else
+    {
+        std::cout << "Hello!\n";
+    }
+    return 0;
+}
